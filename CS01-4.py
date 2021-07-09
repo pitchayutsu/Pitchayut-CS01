94b1a099944d08274c69b6aa2317a1897fa7ea18
pitchayutsu <37756@virtual.prc.ac.th>
2021-07-09 09:41:48 +0700

    So i've updated the simple maths
    more luxurious

--- CS01-4.py
+++ CS01-4.py
@@ -1,7 +1,9 @@
+print("Stating A as 100 and B as 5")
+print("Calculating using simple mathematic functions")
 A = 100
 B = 5
 C = A+B
 D = A-B
 E = A*B
 F = A/B
-print(C,D,E,F)
+print("The answers are",C,D,E,F)
