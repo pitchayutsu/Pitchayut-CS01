5a0f13cd128ac180fd9a90a13eb29b1e452c87ca
pitchayutsu <37756@virtual.prc.ac.th>
2021-07-09 09:44:32 +0700

    added even more text into maths code
    luxurious requiem

--- CS01-4.py
+++ CS01-4.py
@@ -1,9 +1,13 @@
 print("Stating A as 100 and B as 5")
-print("Calculating using simple mathematic functions")
+print("Calculating using simple math functions")
 A = 100
 B = 5
 C = A+B
 D = A-B
 E = A*B
 F = A/B
+print("A+B =",C)
+print("A-B =",D)
+print("A*B =",E)
+print("A/B =",F)
 print("The answers are",C,D,E,F)
