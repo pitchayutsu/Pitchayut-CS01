4d43a5c7666376eb646c1e7e2bee16801487a09d
pitchayutsu <37756@virtual.prc.ac.th>
2021-07-16 22:33:33 +0700

    Added grading code

---
+++ CS01-Grading.py
@@ -0,0 +1,18 @@
+A = int(input("กรอกคะแนนตรงนี้:"))
+
+if (A>=80):
+    print("A")
+elif (A>=75):
+    print("B+")
+elif (A>=70):
+    print("B")
+elif (A>=65):
+    print("C+")
+elif (A>=60):
+    print("C")
+elif (A>=55):
+    print("D+")
+elif (A>=50):
+    print("D")
+else:
+    print("F")
